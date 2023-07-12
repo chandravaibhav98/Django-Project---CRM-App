13604f79400db5d664ad6f08d673aa7b15303895
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-12 20:33:13 +0530

    customer_record
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -35,14 +35,15 @@ def register_user(request):
 
 
 def customer_record(request, pk):
-    if request.user.is_authenticated():
+    if request.user.is_authenticated:
         # Look Up Records
         customer_record = Record.objects.get(id=pk)
         return render(request, 'register.html', {'customer_record': customer_record})
     else:
         messages.success(
-                request, "You must be logged in to see this page")
-            return redirect('home')
+            request, "You must be logged in to see this page")
+        return redirect('home')
+
 
 def home(request):
     records = Record.objects.all()
