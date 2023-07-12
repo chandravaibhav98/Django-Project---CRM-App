95ce30a16fac06db2452657e44a99bacd52a98d6
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-12 17:15:32 +0530

    Sign-Up Form
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- dcrm/website/forms.py
+++ dcrm/website/forms.py
@@ -5,6 +5,8 @@ from django import forms
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(
-        label='', widget=forms.TextInput(attrs={}), required=False)
-    first_name = forms.CharField(label='',)
-    last_name = forms.CharField(label='',)
+        label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+    first_name = forms.CharField(label='', max_length=20, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'first name'}))
+    last_name = forms.CharField(label='', max_length=20, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'last name'}))
