27c82dbc4f3715f43b3542cee2a23a91effac4b3
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-12 19:08:59 +0530

    view records on screen
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -3,23 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
-
-
-def login_user(request):
-    # Check to see if logged in
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        # Authenticate
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, 'You are logged in')
-            return redirect('home')
-        else:
-            messages.success(request, 'Error; Please try Login again')
 
 
 def logout_user(request):
@@ -49,5 +35,20 @@ def register_user(request):
 
 
 def home(request):
-    login_user(request)
-    return render(request, 'home.html', {})
+    records = Record.objects.all()
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(
+                request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'records': records})
